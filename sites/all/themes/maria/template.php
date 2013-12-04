c85e9e0e8adced0cb1716e28224bb1f720480057
Dmitriy Pokhodenko <dmitriy.pokhodenko@wonga.com>
2013-12-04 15:13:08 +0200

    Removed core css files from maria theme.

--- sites/all/themes/maria/template.php
+++ sites/all/themes/maria/template.php
@@ -1,1 +1,17 @@
 <?php
+
+function maria_preprocess_page(&$vars) {
+  $css = $vars['css'];
+  unset($css['all']['module']['modules/node/node.css']);
+  unset($css['all']['module']['modules/user/user.css']);
+  unset($css['all']['module']['modules/system/system.css']);
+  unset($css['all']['module']['modules/system/system-menus.css']);
+  unset($css['all']['module']['modules/system/defaults.css']);
+  unset($css['all']['module']['sites/all/modules/cck/theme/content-module.css']);
+  unset($css['all']['module']['sites/all/modules/cck/modules/fieldgroup/fieldgroup.css']);
+  unset($css['all']['module']['sites/all/modules/filefield/filefield.css']);
+  unset($css['all']['module']['modules/system/defaults.css']);
+  unset($css['all']['module']['modules/system/defaults.css']);
+
+  $vars['styles'] = drupal_get_css($css);
+}
